fd4db5931fb94d33d3229765a47fed1a080c0a20
Alex Silva <134613820+lexops@users.noreply.github.com>
2024-01-02 12:10:20 -0300

    Add example AKS

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,39 @@ provider "azurerm" {
 
 }
 
-/*
-resource "azurerm_resource_group" "state-demo-secure" {
-  name     = "state-demo"
+resource "azurerm_resource_group" "lexops" {
+  name     = "lexops-resources"
   location = "eastus"
 }
-*/
+
+resource "azurerm_kubernetes_cluster" "lexops" {
+  name                = "lexops-aks1"
+  location            = azurerm_resource_group.lexops.location
+  resource_group_name = azurerm_resource_group.lexops.name
+  dns_prefix          = "lexopsaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.lexops.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.lexops.kube_config_raw
+
+  sensitive = true
+}
